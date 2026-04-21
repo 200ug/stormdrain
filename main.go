900894ca8517501222e04114a4ccf972f76fcc0d
2u <a@dyyni.org>
2026-04-21 22:25:51 +0300

    fix: usage message formatting

--- main.go
+++ main.go
@@ -12,14 +12,14 @@ const versionCode = "v0.1 (2026-04-22)"
 const usage = `usage: stormdrain <command> [flags]
 
 commands:
-  new <profile>				create a new container from a profile
-  enter						attach a shell to a container matching cwd
-  close [name] [-f]			close the container matching cwd or given container name (optionally SIGKILL)
-  rm [name]					remove the container matching cwd or given container name
-  list [-f <filter>] [-s]	list all stormdrain containers (optionl filtering and stats)
-  purge						shut down and delete *all* stormdrain containers
-  help						print this usage message
-  version					print current build version
+  new <profile>             create a new container from a profile
+  enter                     attach a shell to a container matching cwd
+  close [name] [-f]         close the container matching cwd or given container name (optionally SIGKILL)
+  rm [name]                 remove the container matching cwd or given container name
+  list [-f <filter>] [-s]   list all stormdrain containers (optionl filtering and stats)
+  purge                     shut down and delete *all* stormdrain containers
+  help                      print this usage message
+  version                   print current build version
 `
 
 func main() {
